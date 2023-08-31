23ffe86e66d9dfb9232430c59598101b15f44dfc
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2023-08-31 16:34:29 +0200

    Update +page.js

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -3,7 +3,7 @@ export async function load({fetch}) {
     let start = performance.now();
     const res = await fetch('/api/cargo');
     const data = await res.json();
-    const res2 = await fetch('src/data/acronyms.json'); //#~# 'src/data/acronyms.json' for dev | 'acronyms.json' for build
+    const res2 = await fetch('acronyms.json'); //#~# 'src/data/acronyms.json' for dev | 'acronyms.json' for build
     const data2 = await res2.json();
     console.log(performance.now()-start);
     return [data, data2];
